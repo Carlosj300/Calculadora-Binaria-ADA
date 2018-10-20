87e6cce6c9b703d2ea56188fa1ee720c51bd102a
Alexander Álvarez Marques <31989083+alex222en@users.noreply.github.com>
2018-10-20 14:26:50 +0100

    Limpieza del ads para su posterior actualización

--- src/funciones.ads
+++ src/funciones.ads
@@ -6,28 +6,4 @@ package funciones with SPARK_Mode is
    type HBinario is array (0 .. 15) of Binary_Values;
    --Tipos usados en los metodos de conversion de numeros
    
-   --function BintoHex32 (n1 : Binario) return String;
-   -- Método que convierte un número binario a un número hexadecimal (máx 32 bits)
-   
-   --function Hex32toBin (n1 : String) return Binario;
-   -- Método que convierte un número hexadecimal a un número binario (máx 32 bits)
-   
-   function DectoBin (n1 : Integer) return HBinario
-     with
-       Pre =>
-         n1 >= 0;
-   -- Método que convierte un número decimal a un número binario (máx 16 bits)
-   
-   --function BintoDec (n1 : HBinario) return Integer;
-   -- Método que convierte un número binario a un número decimal (máx 16 bits)
-   
-   function DectoHex16 (n1 : Integer) return String
-     with
-       Pre =>
-         n1 >= 0;
-   -- Método que convierte un número decimal a un número hexadecimal (máx 16 bits)
-   
-   --function Hex16toDec (n1 : String) return Integer;
-   -- Método que convierte un número hexadecimal a un número decimal (máx 16 bits)
-   
 end funciones;
